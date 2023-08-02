eebba1dea89d9f55c45e72ff3006d63c95015b61
i3elj <guisande.bernardo@gmail.com>
2023-08-02 18:45:25 -0300

    now the home page accepts query params

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe, Ingredient } from 'src/types';
 
 @Component({
@@ -8,9 +9,24 @@ import { Recipe, Ingredient } from 'src/types';
 })
 export class HomePageComponent implements OnInit {
   recipes: Recipe[] = []
+  name: string = ""
+  ingredients: string = ""
+
+  constructor(private route: ActivatedRoute) {}
 
   async ngOnInit(): Promise<void> {
-    let response = await fetch('http://localhost:8080/api/recipes')
+    let name = ""
+    let ingredients = ""
+
+    this.route.queryParams
+      .subscribe(params => {
+        name = params['name'] != undefined ? params['name'] : ''
+        ingredients = params['ingredients'] != undefined ? params['ingredients'] : ''
+      })
+
+    let url = `http://localhost:8080/api/recipes?${name != '' ? "name=" + name : ''}${ingredients != '' ? "ingredients=" + ingredients : ''}`
+
+    let response = await fetch(url)
     this.recipes = await response.json()
   }
 }
